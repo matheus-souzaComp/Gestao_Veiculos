83ee01f7081d0cd5152b617dfa7238288458dc43
Matheus <matheus-souza74@hotmail.com>
2023-11-28 21:25:22 -0300

    atualizacao de ip 5

--- gestaoveiculos/settings.py
+++ gestaoveiculos/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-_dl5qc9&6hxu*jchf1dl8xo^jp5=n4np_rj23auzs=rz5s_0bf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ec2-54-236-7-38.compute-1.amazonaws.com","127.0.0.1"]
+ALLOWED_HOSTS = ["44.219.200.135","127.0.0.1"]
 
 
 # Application definition
